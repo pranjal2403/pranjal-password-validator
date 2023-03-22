f2a349bc0432c7d931ce2b1d1b1f8cf5f18315a1
pranjal2403 <pranjalsingh@cedcoss.com>
2023-03-22 18:42:27 +0530

    done

---
+++ src/JS/main.js
@@ -0,0 +1,7 @@
+let arr=[];
+function display() {
+    let input = document.getElementById("input").Value;
+    let obj = {input:input}
+    console.log(obj)
+    let msg = document.getElementById("message").value;
+}
